cfee443c977034e1f816eca1fea87804acf1a850
sine/cosine/tangent <zfshmn@gmail.com>
2018-11-22 14:20:14 -0800

    can now violently crash client

--- app/src/main/java/com/pylons/wallet/WalletService.kt
+++ app/src/main/java/com/pylons/wallet/WalletService.kt
@@ -1,28 +1,54 @@
 package com.pylons.wallet
 
-import android.app.IntentService
+import android.app.*
+import android.content.Context
 import android.content.Intent
+import android.os.Build
+import android.support.annotation.RequiresApi
+import android.support.v4.app.NotificationCompat
+import android.util.Log
+
+const val CHANNEL_ID = "PYLONS_WALLET"
 
 class WalletService : IntentService("WalletService") {
+
     override fun onHandleIntent(intent: Intent?) {
+        Log.i("onHandleIntent", intent!!.action)
         when (intent?.action) {
             ACTION_INVOKE_WALLET_SERVICE -> resolvePylonsAction(intent)
         }
+    }
+
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        Log.i("foo", "bar")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val nm = applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            nm.createNotificationChannel(NotificationChannel(CHANNEL_ID, "pylons_wallet",NotificationManager.IMPORTANCE_DEFAULT))
+        }
+        startForeground(9, NotificationCompat.Builder(this, CHANNEL_ID).setSmallIcon(R.drawable.ic_wallet_service).setAutoCancel(true).build())
+        Log.i("foo", "bar-bar binks")
+        return super.onStartCommand(intent, flags, startId)
     }
 
     fun resolvePylonsAction(intent: Intent) {
         var pylonsAction = intent.getStringExtra("pylonsAction")
         when (pylonsAction) {
             "WALLET_SERVICE_TEST" -> {
+                Log.i("info", "Returning data to client (no UI transfer needed)")
                 val successIntent = Intent(ACTION_RETURN_TO_CLIENT)
                 successIntent.putExtra("info", "it works i guess")
-                sendBroadcast(successIntent)
+                val innerIntent = Intent(ACTION_PASS_DATA_TO_SHIM)
+                innerIntent.putExtra("_@PINNER", successIntent)
+                val pending = intent.getParcelableExtra<PendingIntent>("_@PPENDING");
+                pending.send(applicationContext, 0, innerIntent)
             }
             "WALLET_UI_TEST" -> requiresUI()
         }
     }
 
     fun requiresUI () {
+        Log.i("info", "Asking client to transfer UI control...")
         val needUIIntent = Intent(ACTION_REQUIRE_WALLET_UI)
         sendBroadcast(needUIIntent)
     }
