3f36a2ca7e645eb8324aa1ca5685ddb78388884d
sine/cosine/tangent <zfshmn@gmail.com>
2018-12-07 18:41:42 -0800

    fucking gradle

--- android/app/src/main/java/Action.kt
+++
@@ -1,7 +0,0 @@
-package com.pylons.wallet.app
-
-const val ACTION_INVOKE_WALLET_SERVICE = "com.pylons.action.INVOKE_WALLET"
-const val ACTION_INVOKE_WALLET_UI = "com.pylons.action.INVOKE_WALLET_UI"
-const val ACTION_REQUIRE_WALLET_UI = "com.pylons.action.REQUIRE_WALLET_UI"
-const val ACTION_RETURN_TO_CLIENT = "com.pylons.action.RETURN_TO_CLIENT"
-const val ACTION_PASS_DATA_TO_SHIM = "com.pylons.action.WALLET_SERVICE_RETURN_DATA_TO_CLIENT"
