8399342cca0edfcf05548cdbae581f885376405c
sine/cosine/tangent <zfshmn@gmail.com>
2019-05-23 12:48:51 -0700

    fix to registeprofile testcase

--- test/kotlin/walletcore/internal/ActionResolutionTableTest.kt
+++ test/kotlin/walletcore/internal/ActionResolutionTableTest.kt
@@ -91,7 +91,7 @@ internal class ActionResolutionTableTest {
     fun case_registerProfile () {
         Core.start()
         val successResponse = Response(MessageData(booleans = mutableMapOf(Keys.profileExists to true)), Status.OK_TO_RETURN_TO_CLIENT)
-        val actualResponse = actionResolutionTable(Actions.newProfile, MessageData.empty(), MessageData(strings = mutableMapOf(Keys.name to "fooBar")))
+        val actualResponse = actionResolutionTable(Actions.newProfile, MessageData(strings = mutableMapOf(Keys.name to "fooBar")))
         assertEquals(successResponse.status, actualResponse.status)
         assertEquals(successResponse.msg!!.booleans[Keys.profileExists], actualResponse.msg!!.booleans[Keys.profileExists])
     }
