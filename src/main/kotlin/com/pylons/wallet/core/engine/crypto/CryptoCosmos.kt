472faaf16fde4c53323423785a5f3247842a5850
sine/cosine/tangent <zfshmn@gmail.com>
2019-06-03 21:26:28 -0700

    wip address gen

--- src/main/kotlin/com/pylons/wallet/core/engine/crypto/CryptoCosmos.kt
+++ src/main/kotlin/com/pylons/wallet/core/engine/crypto/CryptoCosmos.kt
@@ -1,11 +1,20 @@
 package com.pylons.wallet.core.engine.crypto
 
+import com.google.common.hash.Hashing
 import com.squareup.moshi.Moshi
 import org.apache.commons.codec.binary.Base32
 import org.apache.tuweni.bytes.Bytes
 import com.pylons.wallet.core.types.UserData
+import org.apache.commons.codec.digest.DigestUtils
 import org.apache.tuweni.bytes.Bytes32
+import org.apache.tuweni.bytes.MutableBytes
+import org.apache.tuweni.bytes.MutableBytes32
 import org.apache.tuweni.crypto.*
+import org.apache.tuweni.crypto.sodium.SHA256Hash
+import org.bouncycastle.jcajce.provider.digest.RIPEMD160
+import java.security.MessageDigest
+
+
 
 internal class CryptoCosmos () : CryptoHandler() {
     override fun getPrefix() : String = "__CRYPTO_COSMOS__"
@@ -28,8 +37,28 @@ internal class CryptoCosmos () : CryptoHandler() {
     override fun getEncodedPublicKey() : String = Base32().encodeToString(keyPair!!.publicKey()!!.bytesArray())
 
     companion object {
-        fun getAddressFromKeyPair (keyPair : SECP256K1.KeyPair) : Bytes32 {
-            throw NotImplementedError()
+        private fun getComprressedPubkey (keyPair: SECP256K1.KeyPair) : Bytes {
+            val ecPoint = keyPair.publicKey().asEcPoint()
+            var xBytes = Bytes.wrap(ecPoint.xCoord.toBigInteger().toByteArray()).trimLeadingZeros()
+            System.out.println(xBytes.toHexString())
+            val yStr = ecPoint.yCoord.toBigInteger().toString()
+            val xStr = ecPoint.xCoord.toBigInteger().toString()
+            val prefix = when (yStr > xStr) {
+                true -> 0x02
+                false -> 0x03
+            }
+            var bytes = MutableBytes.wrap(ByteArray(33))
+            bytes[0] = prefix.toByte()
+            xBytes.copyTo(bytes, 1)
+            System.out.println(bytes.toHexString())
+            return bytes
+        }
+
+        fun getAddressFromKeyPair (keyPair : SECP256K1.KeyPair) : Bytes {
+            val pubkey = getComprressedPubkey(keyPair)
+            val sha = MessageDigest.getInstance("SHA-256").digest(pubkey.toArray())
+            val ripEmd = MessageDigest.getInstance("RIPEMD160").digest(sha)
+            return Bytes.wrap(ripEmd)
         }
     }
 }
--- src/test/kotlin/com/pylons/wallet/core/engine/TendermintAddressTest.kt
+++ src/test/kotlin/com/pylons/wallet/core/engine/TendermintAddressTest.kt
@@ -4,8 +4,13 @@ import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
 import com.pylons.wallet.core.engine.crypto.CryptoCosmos
+import org.apache.tuweni.bytes.Bytes
 import org.apache.tuweni.bytes.Bytes32
 import org.apache.tuweni.crypto.SECP256K1
+import org.bitcoinj.core.Base58
+import org.bouncycastle.jcajce.provider.symmetric.ARC4
+import org.bouncycastle.jce.provider.BouncyCastleProvider
+import java.security.Provider
 
 internal class TendermintAddressTest {
     private val dsPrivate = "a96e62ed3955e65be32703f12d87b6b5cf26039ecfa948dc5107a495418e5330"
@@ -14,9 +19,11 @@ internal class TendermintAddressTest {
 
     @Test
     fun generatesAddress () {
-        var keyPair = SECP256K1.KeyPair.create(
-                SECP256K1.SecretKey.fromBytes(Bytes32.fromHexString(dsPrivate)),
-                SECP256K1.PublicKey.fromBytes(Bytes32.fromHexString(dsPublic)))
-        assertEquals(dsAddr, CryptoCosmos.getAddressFromKeyPair(keyPair).toHexString())
+        java.security.Security.addProvider(BouncyCastleProvider())
+        System.out.println("registered")
+        val privateKey = SECP256K1.SecretKey.fromBytes(Bytes32.fromHexString(dsPrivate))
+        val keyPair = SECP256K1.KeyPair.create(privateKey, SECP256K1.PublicKey.fromSecretKey(privateKey))
+        // TODO: how is this encoded?
+        assertEquals(Base58.decode(dsAddr), CryptoCosmos.getAddressFromKeyPair(keyPair))
     }
 }
