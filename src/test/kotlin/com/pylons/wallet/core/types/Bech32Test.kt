c66fb307192addc9c36cad2a987df4194309c1c8
Michael Sofaer <msofaer@catalist.us>
2019-06-09 16:15:19 -0400

    better failing test

--- src/test/kotlin/com/pylons/wallet/core/types/Bech32Test.kt
+++ src/test/kotlin/com/pylons/wallet/core/types/Bech32Test.kt
@@ -1,13 +1,23 @@
 package com.pylons.wallet.core.types
 
+import org.bitcoinj.core.Bech32
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
 
 internal class PylonsBech32Test {
 
+    val key = "A7AqbjDky/lrA1bj7bEW3vww8Kc4IDXIb76w3Zg+DTKj"
+    val addr = "cosmos146yrz0p79pm6xd33nr3ajtxv2206rvcx0rs2c6"
+
+    @Test
+    fun decode(){
+        val decoded = Bech32.decode(addr)
+        assertEquals(key, decoded)
+    }
+
     @Test
     fun fromKey() {
-        assertEquals(PylonsBech32().fromKey("A7AqbjDky/lrA1bj7bEW3vww8Kc4IDXIb76w3Zg+DTKj"), "cosmos146yrz0p79pm6xd33nr3ajtxv2206rvcx0rs2c6")
+        assertEquals(PylonsBech32().fromKey(key), addr)
     }
 }
