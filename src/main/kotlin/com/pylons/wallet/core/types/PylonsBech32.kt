e5879343470d83d2aa6ef1ff1c1a76c6ecf979de
Michael Sofaer <msofaer@catalist.us>
2019-06-09 16:04:40 -0400

    better broken

--- src/main/kotlin/com/pylons/wallet/core/types/PylonsBech32.kt
+++ src/main/kotlin/com/pylons/wallet/core/types/PylonsBech32.kt
@@ -1,7 +1,7 @@
 package com.pylons.wallet.core.types
 
 internal class PylonsBech32 {
-    fun fromKey(key : CharArray) : String {
+    fun fromKey(key : String) : String {
         return key.toString()
     }
 }
--- src/test/kotlin/com/pylons/wallet/core/types/Bech32Test.kt
+++ src/test/kotlin/com/pylons/wallet/core/types/Bech32Test.kt
@@ -8,6 +8,6 @@ internal class PylonsBech32Test {
 
     @Test
     fun fromKey() {
-        assertEquals("A7AqbjDky/lrA1bj7bEW3vww8Kc4IDXIb76w3Zg+DTKj", "cosmos146yrz0p79pm6xd33nr3ajtxv2206rvcx0rs2c6")
+        assertEquals(PylonsBech32().fromKey("A7AqbjDky/lrA1bj7bEW3vww8Kc4IDXIb76w3Zg+DTKj"), "cosmos146yrz0p79pm6xd33nr3ajtxv2206rvcx0rs2c6")
     }
 }
