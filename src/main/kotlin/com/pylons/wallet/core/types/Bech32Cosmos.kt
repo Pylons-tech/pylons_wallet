34ba47a425529b184e34bfc68d2d7db1fa01b408
sine/cosine/tangent <zfshmn@gmail.com>
2019-06-10 14:21:01 -0700

    nasty one-to-one port of bech32.decode

--- src/main/kotlin/com/pylons/wallet/core/types/Bech32Cosmos.kt
+++ src/main/kotlin/com/pylons/wallet/core/types/Bech32Cosmos.kt
@@ -43,33 +43,41 @@ internal class Bech32Cosmos {
                 throw Exception("Bit groups must be between 1 and 8, but provided were fromBits:" +
                         "$fromBits and toBits: $toBits")
             }
+
             // Final bytes, each byte encoding toBits bits
             var regrouped : MutableList<Byte> = mutableListOf()
+
             // Next byte to be created
             var nextByte = 0.toByte()
+
             // Number of bits added to nextByte (up to goal of toBits)
             var filledBits = 0.toByte()
 
             data.forEach{
                 // Discard unused bits
                 var b = it.shr(8 - fromBits)
+
                 // Bits remaining to extract from input data
                 var remFromBits = fromBits
                 while (remFromBits > 0) {
                     // Bits remaining to add to next byte
                     var remToBits = (toBits - filledBits).toByte()
+
                     // Number of bytes to extract next.
                     // The lesser of remToBits and remFromBits.
                     var toExtract = when (remToBits < remFromBits) {
                         true -> remToBits
                         false -> remFromBits
                     }
+
                     // Add the next bits to nextByte, shifting the already-added bits left.
                     nextByte = (nextByte.shl(toExtract)).or(b.shl(8 - toExtract)).toByte()
+
                     // Discard the bits we just extracted and get ready for next iteration.
                     b = b.shl(toExtract).toByte()
                     remFromBits = (remFromBits - toExtract).toByte()
                     filledBits = (filledBits + toExtract).toByte()
+
                     // If nextByte is completely filled, add it to our regrouped bytes and start on the next byte.
                     if (filledBits == toBits) {
                         regrouped.add(nextByte)
@@ -78,6 +86,7 @@ internal class Bech32Cosmos {
                     }
                 }
             }
+
             // Pad an unfinished group if specified (and group is unfinished)
             if (pad && filledBits > 0) {
                 nextByte = nextByte.shl((toBits - filledBits))
@@ -85,15 +94,59 @@ internal class Bech32Cosmos {
                 filledBits = 0
                 nextByte = 0
             }
+
             // Any incomplete group must be <= 4 bits, and all zeroes.
             if (filledBits > 0 && (filledBits > 4 || nextByte != 0.toByte())) {
                 throw Exception("Invalid incomplete group")
             }
+
             return regrouped.toByteArray()
         }
 
-        fun decode (input : String) : Bech32Data {
-            throw NotImplementedError()
+        fun decode (input : String) : UnencodedBech32Data {
+            var bech = input
+
+            // The maximum allowed length for a bech32 string is 90. It must also
+            // be at least 8 characters, since it needs a non-empty HRP, a
+            // separator, and a 6 character checksum.
+            if (bech.length < 8 || bech.length > 90) throw Exception("Invalid Bech32 string length ${bech.length}")
+
+            // Only	ASCII characters between 33 and 126 are allowed.
+            for (i in 0..bech.length) {
+                if (bech[i] < 33.toChar() || bech[i] > 126.toChar()) throw Exception("invalid character in string: ${bech[i]}")
+            }
+
+            // The characters must be either all lowercase or all uppercase.
+            val lower = bech.toLowerCase()
+            val upper = bech.toUpperCase()
+            if (bech != lower && bech != upper) throw Exception("String not all lowercase or all uppercase")
+            bech = lower
+
+            // The string is invalid if the last '1' is non-existent, the first character of the string
+            // (no human-readable part), or one of the last 6 characters of the string
+            // (since checksum cannot contain '1').
+            val one = bech.lastIndexOf('1')
+            if (one < 1 || one+7 > bech.length) throw Exception("Index of last '1' in string is $one, which is invalid")
+            // The human-readable part is everything before the last '1'.
+            val hrp = bech.substring(0, one)
+            val data = bech.substring(one + 1)
+
+            // Each character corresponds to the byte with value of the index in
+            // 'charset'.
+            val decoded = toBytes(data)
+
+            // TODO: I'm not actually sure the array slicing here is right.
+            //  We'll be porting the tests, too, so we'll see then.
+
+            if (!bech32VerifyChecksum(hrp, decoded)) {
+                val checksum = bech.substring(bech.length-6)
+                val expected = toChars(bech32Checksum(hrp,
+                        decoded.slice(0..decoded.size-6).toByteArray()))
+                throw Exception("Checksum failed. Expected: $expected, got: $checksum")
+            }
+
+            // We exclude the last 6 bytes, which is the checksum.
+            return UnencodedBech32Data(decoded.slice(0..decoded.size-6).toByteArray(), hrp)
         }
 
         fun decodeAndConvert (bech : String) : UnencodedBech32Data {
@@ -103,6 +156,22 @@ internal class Bech32Cosmos {
         }
 
         fun encode (hrp : String, data : ByteArray) : String{
+            throw NotImplementedError()
+        }
+
+        fun bech32VerifyChecksum(hrp : String, data : ByteArray) : Boolean {
+            throw NotImplementedError()
+        }
+
+        fun bech32Checksum(hrp : String, data : ByteArray) : ByteArray {
+            throw NotImplementedError()
+        }
+
+        fun toBytes (chars : String) : ByteArray {
+            throw NotImplementedError()
+        }
+
+        fun toChars (data : ByteArray) : String {
             throw NotImplementedError()
         }
 
