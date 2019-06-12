7f1da5ba5e68e60e0c8660d4a18a5c18f35cc1ac
sine/cosine/tangent <zfshmn@gmail.com>
2019-06-12 12:23:21 -0700

    infix tests + ubyte support

--- src/main/kotlin/com/pylons/wallet/core/Infixes.kt
+++ src/main/kotlin/com/pylons/wallet/core/Infixes.kt
@@ -12,3 +12,13 @@ infix inline fun Byte.shr(that: Byte): Byte = (this.toInt().shr(that.toInt())).t
 infix inline fun Byte.or(that: Int): Byte = (this.toInt().and(that)).toByte()
 infix inline fun Int.or(that: Byte): Byte = (this.and(that.toInt())).toByte()
 infix inline fun Byte.or(that: Byte): Byte = (this.toInt().and(that.toInt())).toByte()
+
+infix inline fun UByte.shl(that: Int): UByte = (this.toInt().shl(that)).toUByte()
+infix inline fun Int.shl(that: UByte): UByte = (this.shl(that.toInt())).toUByte()
+infix inline fun UByte.shl(that: UByte): UByte = (this.toInt().shl(that.toInt())).toUByte()
+infix inline fun UByte.shr(that: Int): UByte = (this.toInt().shr(that)).toUByte()
+infix inline fun Int.shr(that: UByte): UByte = (this.shr(that.toInt())).toUByte()
+infix inline fun UByte.shr(that: UByte): UByte = (this.toInt().shr(that.toInt())).toUByte()
+infix inline fun UByte.or(that: Int): UByte = (this.toInt().and(that)).toUByte()
+infix inline fun Int.or(that: UByte): UByte = (this.and(that.toInt())).toUByte()
+infix inline fun UByte.or(that: UByte): UByte = (this.toInt().and(that.toInt())).toUByte()
---
+++ src/test/kotlin/com/pylons/wallet/core/InfixTest.kt
@@ -0,0 +1,41 @@
+package com.pylons.wallet.core
+
+import com.pylons.wallet.core.Infixes.shl
+import com.pylons.wallet.core.Infixes.shr
+import org.junit.jupiter.api.*
+import org.junit.jupiter.api.Assertions.*
+
+internal class InfixTest {
+    val shl = mapOf(
+            // 0111 1111 to 1111 1110
+            127.toUByte() to 254.toUByte(),
+            // 0000 0001 to 0000 0010
+            1.toUByte() to 2.toUByte(),
+            // 1111 1111 to 1111 1110
+            255.toUByte() to 254.toUByte(),
+            // 1000 0000 to 0000 0000
+            128.toUByte() to 0.toUByte()
+    )
+
+    val shr = mapOf(
+            // 1111 1110 to 0111 1111
+            254.toUByte() to 127.toUByte(),
+            // 0000 0010 to 0000 0001
+            2.toUByte() to 1.toUByte(),
+            // 1111 1110 to 0111 1111
+            255.toUByte() to 127.toUByte(),
+            // 1000 0000 to 0100 0000
+            128.toUByte() to 64.toUByte()
+    )
+
+
+    @Test
+    fun bitshiftOnUBytes () {
+        for (test in shl) assertEquals(test.key.shl(1), test.value,
+                "Left shift on ${test.key} yielded ${test.key.shl(1)}, expected " +
+                        "${test.value}")
+        for (test in shr) assertEquals(test.key.shr(1), test.value,
+                "Right shift on ${test.key} yielded ${test.key.shr(1)}, expected " +
+                        "${test.value}")
+    }
+}
