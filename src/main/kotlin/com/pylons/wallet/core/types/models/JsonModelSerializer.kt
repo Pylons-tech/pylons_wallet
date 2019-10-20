9090b876aaf26ef34751a2604af0ead1786754fb
sine/cosine/tangent <zfshmn@gmail.com>
2019-10-19 18:51:01 -0700

    this is hacky and bad but it gets around jvm generics being shit and lets us deal w/ arrays in a sensible manner

--- src/main/kotlin/com/pylons/wallet/core/types/models/JsonModelSerializer.kt
+++ src/main/kotlin/com/pylons/wallet/core/types/models/JsonModelSerializer.kt
@@ -2,31 +2,24 @@ package com.pylons.wallet.core.types.models
 
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonWriter
-import com.sun.org.apache.xpath.internal.operations.Bool
 import kotlin.reflect.full.declaredMemberProperties
 import kotlin.reflect.full.findAnnotation
-import kotlin.reflect.full.isSubtypeOf
-import kotlin.reflect.full.memberProperties
-import kotlin.reflect.jvm.internal.impl.load.kotlin.JvmType
 
 object JsonModelSerializer {
 
     fun serialize(p0: JsonWriter, p1: Any?) {
         if (p1 != null) {
             val kClass = p1::class
-            kClass.declaredMemberProperties.forEach {
-                val json = it.findAnnotation<Json>()
+            p0.beginObject()
+            kClass.declaredMemberProperties.forEach { prop ->
+                val json = prop.findAnnotation<Json>()
                 if (json != null) {
-                    p0.beginObject()
-                    var value = it.getter.call(p1)
+                    var value = prop.getter.call(p1)
                     // Because we have to strip the whitespaces from the string this spits out by hand,
                     // we're doing some Bullshit here to protect whitespaces within our data.
-                    if (it.returnType == String::class.java) value = protectWhitespace(value.toString())
+                    if (prop.returnType == String::class.java) value = protectWhitespace(value.toString())
                     p0.name(json.name)
-                    println(it.returnType)
-                    println(String::class)
-
-                    when (it.returnType.classifier) {
+                    when (prop.returnType.classifier) {
                         Byte::class -> p0.value(value as Byte)
                         Int::class -> p0.value(value as Int)
                         Long::class -> p0.value(value as Long)
@@ -34,28 +27,23 @@ object JsonModelSerializer {
                         Float::class -> p0.value(value as Float)
                         Double::class -> p0.value(value as Double)
                         Boolean::class -> p0.value(value as Boolean)
-                        String::class -> {
-                            println("oas")
-                            p0.value(value as String)
-                            println(value as String)
-                            println("oas")
-                        }
+                        String::class -> p0.value(value as String)
                         // else: arrays/maps/objects...
                         else -> {
                             when {
                                 value == null -> p0.nullValue()
-                                it.returnType.javaClass.isArray -> {
+                                Regex("kotlin.Array<.*>").matches(prop.returnType.toString()) -> {
                                     p0.beginArray()
-                                    when (it.returnType.classifier) {
-                                        Array<String>::class -> (it as Array<String>).forEach { p0.value(protectWhitespace(it)) }
-                                        Array<Byte>::class -> (it as Array<Byte>).forEach { p0.value(it) }
-                                        Array<Int>::class -> (it as Array<Int>).forEach { p0.value(it) }
-                                        Array<Long>::class -> (it as Array<Long>).forEach { p0.value(it) }
-                                        Array<Number>::class -> (it as Array<Number>).forEach { p0.value(it) }
-                                        Array<Float>::class -> (it as Array<Float>).forEach { p0.value(it) }
-                                        Array<Double>::class -> (it as Array<Double>).forEach { p0.value(it) }
-                                        Array<Boolean>::class -> (it as Array<Boolean>).forEach { p0.value(it) }
-                                        else -> (it as Array<*>).forEach { serialize(p0, it) }
+                                    when (prop.returnType.toString()) {
+                                        "kotlin.Array<kotlin.String>" -> (value as Array<String>).forEach { p0.value(protectWhitespace(it)) }
+                                        "kotlin.Array<kotlin.Byte>" -> (value as Array<Byte>).forEach { p0.value(it) }
+                                        "kotlin.Array<kotlin.Int>" -> (value as Array<Int>).forEach { p0.value(it) }
+                                        "kotlin.Array<kotlin.Long>" -> (value as Array<Long>).forEach { p0.value(it) }
+                                        "kotlin.Array<kotlin.Number>" -> (value as Array<Number>).forEach { p0.value(it) }
+                                        "kotlin.Array<kotlin.Float>" -> (value as Array<Float>).forEach { p0.value(it) }
+                                        "kotlin.Array<kotlin.Double>" -> (value as Array<Double>).forEach { p0.value(it) }
+                                        "kotlin.Array<kotlin.Boolean>" -> (value as Array<Boolean>).forEach { p0.value(it) }
+                                        else -> (prop as Array<*>).forEach { serialize(p0, it) }
                                     }
                                     p0.endArray()
                                 }
@@ -65,9 +53,10 @@ object JsonModelSerializer {
                             }
                         }
                     }
-                    p0.endObject()
+
                 }
             }
+            p0.endObject()
         }
         else p0.nullValue()
         p0.flush()
--- src/test/kotlin/com/pylons/wallet/core/types/models/JsonModelSerializerTest.kt
+++ src/test/kotlin/com/pylons/wallet/core/types/models/JsonModelSerializerTest.kt
@@ -13,15 +13,21 @@ import org.junit.jupiter.api.Assertions.*
 class TestObject {
     @Json(name = "bleh_prop")
     val bleh : String = "asdfjkl;"
+    val not_present : String = "no json prop so this shouldn't show"
+    @Json(name = "byte")
+    val byte : Byte = 3
+    @Json(name = "int_array")
+    val intArray : Array<Int> = arrayOf(1, 2, 3, 4, 5)
 }
 
 class JsonModelSerializerTest {
     @Test
     fun a () {
         val stream = ByteArrayOutputStream()
-        val expected = """{"bleh_prop":"asdfjkl;"}"""
+        val expected = """{"bleh_prop":"asdfjkl;","byte":3,"int_array":[1,2,3,4,5]}"""
         JsonModelSerializer.serialize(JsonWriter.of(Okio.buffer(Okio.sink(stream))), TestObject())
         val actual = stream.toString()
+        println(actual)
         assertEquals(expected, actual)
     }
 
