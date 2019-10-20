eb8ffc27887713f1c99de055e899fb58e4e1fbd0
sine/cosine/tangent <zfshmn@gmail.com>
2019-10-19 22:04:49 -0700

    and nesting is still borked

--- src/test/kotlin/com/pylons/wallet/core/types/models/JsonModelSerializerTest.kt
+++ src/test/kotlin/com/pylons/wallet/core/types/models/JsonModelSerializerTest.kt
@@ -3,6 +3,7 @@ package com.pylons.wallet.core.types.models
 import com.google.common.io.CharStreams
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonWriter
+import com.sun.org.apache.xpath.internal.operations.Bool
 import okio.BufferedSink
 import okio.Okio
 import java.io.*
@@ -18,13 +19,25 @@ class TestObject {
     val byte : Byte = 3
     @Json(name = "int_array")
     val intArray : Array<Int> = arrayOf(1, 2, 3, 4, 5)
+    @Json(name = "inner_1")
+    val inner1 = InnerObject("first")
+    @Json(name = "inner_2")
+    val inner2 = InnerObject("second")
+}
+
+class InnerObject (
+        @Json(name = "name")
+        val name : String) {
+    @Json(name = "nested")
+    val nested : Boolean = true
 }
 
 class JsonModelSerializerTest {
     @Test
     fun a () {
         val stream = ByteArrayOutputStream()
-        val expected = """{"bleh_prop":"asdfjkl;","byte":3,"int_array":[1,2,3,4,5]}"""
+        val expected =
+                """{"bleh_prop":"asdfjkl;","byte":3,"int_array":[1,2,3,4,5],"inner_1":{"name":"first","nested":true},"inner_2":{"name:"second","nested":true}}"""
         JsonModelSerializer.serialize(JsonWriter.of(Okio.buffer(Okio.sink(stream))), TestObject())
         val actual = stream.toString()
         println(actual)
