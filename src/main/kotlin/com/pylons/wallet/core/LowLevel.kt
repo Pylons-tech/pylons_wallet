276562c6f70e88938e612eaec805f3d7aa6d3b3e
sine/cosine/tangent <zfshmn@gmail.com>
2020-02-14 22:06:11 -0800

    lmao

--- src/main/kotlin/com/pylons/wallet/core/LowLevel.kt
+++ src/main/kotlin/com/pylons/wallet/core/LowLevel.kt
@@ -3,7 +3,6 @@ package com.pylons.wallet.core
 import com.pylons.wallet.core.engine.TxPylonsEngine
 import com.pylons.wallet.core.types.AccAddress
 import com.pylons.wallet.core.types.SECP256K1
-import com.pylons.wallet.core.types.jsonTemplate.baseJsonWeldFlow
 import com.pylons.wallet.core.types.tx.msg.Msg
 import org.apache.commons.codec.binary.Hex.*
 import org.apache.tuweni.bytes.Bytes32
@@ -19,20 +18,23 @@ object LowLevel {
         }
     }
 
-    fun getSignedTx (privkeyHex : String, accountNumber : Long, sequence : Long, msgJson : String) : String {
+    private fun setup (privkeyHex : String, accountNumber : Long, sequence : Long) {
         Core.forceKeys(privkeyHex, AccAddress.getAddressFromNode(nodeUrl,
                 SECP256K1.KeyPair.fromSecretKey(SECP256K1.SecretKey.fromBytes(Bytes32.wrap(decodeHex(privkeyHex))))))
-        val cryptoCosmos = (Core.engine as TxPylonsEngine).cryptoCosmos
+        (Core.userProfile!!.credentials as TxPylonsEngine.Credentials).accountNumber = accountNumber
+        (Core.userProfile!!.credentials as TxPylonsEngine.Credentials).sequence = sequence
+    }
+
+    fun getSignedTx (privkeyHex : String, accountNumber : Long, sequence : Long, msgJson : String) : String {
+        setup(privkeyHex, accountNumber, sequence)
         return when (val msg = Msg.fromJson(msgJson)) {
             null -> "invalid message"
-            else -> baseJsonWeldFlow(msgJson, msg.toSignStruct(), accountNumber, sequence,
-                    cryptoCosmos.keyPair!!.publicKey())
+            else -> msg.toSignedTx()
         }
     }
 
     fun getSignBytes (privkeyHex : String, accountNumber : Long, sequence : Long, msgJson : String) : String {
-        Core.forceKeys(privkeyHex, AccAddress.getAddressFromNode(getUrl(),
-                SECP256K1.KeyPair.fromSecretKey(SECP256K1.SecretKey.fromBytes(Bytes32.wrap(decodeHex(privkeyHex))))))
+        setup(privkeyHex, accountNumber, sequence)
         return when (val msg = Msg.fromJson(msgJson)) {
             null -> "invalid message"
             else -> msg.toSignStruct()
