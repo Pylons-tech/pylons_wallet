78e116c76600e1269c763e4b631ed620842bd949
Tiny <development0305@gmail.com>
2021-03-23 09:13:26 +0800

    IPC abstraction layer for Android Wallet, and some refactoring.

--- libkylons/src/main/kotlin/com/pylons/lib/Wallet.kt
+++ libkylons/src/main/kotlin/com/pylons/lib/Wallet.kt
@@ -5,6 +5,7 @@ import com.pylons.ipc.Message
 import com.pylons.lib.types.*
 import com.pylons.lib.types.tx.Coin
 import com.pylons.lib.types.tx.Trade
+import com.pylons.lib.types.tx.TxData
 import com.pylons.lib.types.tx.item.Item
 import com.pylons.lib.types.tx.recipe.CoinInput
 import com.pylons.lib.types.tx.recipe.EntriesList
@@ -23,38 +24,45 @@ abstract class Wallet {
      * Signature for the method what we call to pass messages into the
      * wallet. IPC happens after this implementation.
      */
-    protected abstract fun <T> sendMessage(message: Message, callback : (T) -> Unit)
+    protected abstract fun <T> sendMessage(message: Message, callback: (T) -> Unit)
 
     /**
      * True if an IPC target exists; false otherwise.
      */
-    abstract fun exists (callback : (Boolean) -> Unit)
+    abstract fun exists(callback: (Boolean) -> Unit)
 
     /**
      * Get current profile, or null if none exists.
      */
-    fun fetchProfile (callback: (Profile?) -> Unit) {
-        sendMessage<Profile?>(Message.GetProfile()) {callback(it)}
+    fun fetchProfile(callback: (Profile?) -> Unit) {
+        sendMessage<Profile?>(Message.GetProfile()) { callback(it) }
     }
 
     /**
      * Get a list of all items owned by current profile.
      */
     fun listItems(callback: (List<Item>) -> Unit) {
-        sendMessage<Profile?>(Message.GetProfile()) {callback(it?.items.orEmpty())}
+        sendMessage<Profile?>(Message.GetProfile()) { callback(it?.items.orEmpty()) }
     }
 
     /**
      * Register a new profile.
      */
-    fun registerProfile (callback: (Profile?) -> Unit) {
-        sendMessage<Profile?>(Message.RegisterProfile()) {callback(it)}
+    fun registerProfile(callback: (Profile?) -> Unit) {
+        sendMessage<Profile?>(Message.RegisterProfile()) { callback(it) }
     }
 
-    fun placeForSale (item : Item, price : Long, callback: (Transaction?) -> Unit) {
-        sendMessage<Transaction?>(Message.CreateTrade(listOf(
-            klaxon.toJsonString(Coin("pylon", price))),
-            listOf(), listOf(), listOf(item.id))) {callback(it)}
+    fun placeForSale(item: Item, price: Long, callback: (Transaction?) -> Unit) {
+        sendMessage<Transaction?>(
+            Message.CreateTrade(
+                listOf(
+                    klaxon.toJsonString(Coin("pylon", price))
+                ),
+                listOf(),
+                listOf(),
+                listOf(item.id)
+            )
+        ) { callback(it) }
     }
 
     fun getTrades(callback: (List<Trade>?) -> Unit) {
@@ -63,26 +71,43 @@ abstract class Wallet {
         throw NotImplementedError()
     }
 
-    fun buyItem (trade : Trade, callback: (Transaction?) -> Unit) {
-        sendMessage<Transaction?>(Message.FulfillTrade(trade.id)) {callback(it)}
+    fun buyItem(trade: Trade, callback: (Transaction?) -> Unit) {
+        sendMessage<Transaction?>(Message.FulfillTrade(trade.id)) { callback(it) }
     }
 
-    fun createRecipe(name : String, cookbook : String, description : String,
-                     blockInterval : Long, coinInputs : List<CoinInput>,
-                     itemInputs: List<ItemInput>, outputTable : EntriesList,
-                     outputs : List<WeightedOutput>, callback: (Transaction?) -> Unit) {
-        sendMessage<Transaction>(Message.CreateRecipes(listOf(name), listOf(cookbook), listOf(description),
-        listOf(blockInterval), listOf(klaxon.toJsonString(coinInputs)), listOf(klaxon.toJsonString(itemInputs)),
-        listOf(klaxon.toJsonString(outputTable)), listOf(klaxon.toJsonString(outputs)))) {callback(it)}
+    fun createRecipe(
+        name: String,
+        cookbook: String,
+        description: String,
+        blockInterval: Long,
+        coinInputs: List<CoinInput>,
+        itemInputs: List<ItemInput>,
+        outputTable: EntriesList,
+        outputs: List<WeightedOutput>,
+        callback: (Transaction?) -> Unit
+    ) {
+        sendMessage <Transaction>(
+            Message.CreateRecipes(
+                listOf(name),
+                listOf(cookbook),
+                listOf(description),
+                listOf(blockInterval),
+                listOf(klaxon.toJsonString(coinInputs)),
+                listOf(klaxon.toJsonString(itemInputs)),
+                listOf(klaxon.toJsonString(outputTable)),
+                listOf(klaxon.toJsonString(outputs))
+            ),
+            callback
+        )
     }
 
-    class Android : Wallet(){
+    class Android : Wallet() {
         override fun <T> sendMessage(message: Message, callback: (T) -> Unit) {
-            TODO("Not yet implemented")
+            callback(Transaction(TxData(klaxon.toJsonString(message), "", listOf())) as T)
         }
 
         override fun exists(callback: (Boolean) -> Unit) {
-            TODO("Not yet implemented")
+
         }
     }
 
