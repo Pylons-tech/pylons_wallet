adea67b2584ddf87ca83be5c7bdbee674c400dd0
Tiny <development0305@gmail.com>
2021-04-06 13:58:47 +0800

    Update ClientIpcHelper.kt

--- libkylons/src/main/kotlin/com/pylons/ipc/ClientIpcHelper.kt
+++ libkylons/src/main/kotlin/com/pylons/ipc/ClientIpcHelper.kt
@@ -10,15 +10,16 @@ abstract class ClientIpcHelper {
     protected abstract fun writeString(s: String)
 
     companion object {
-        val IMPLEMENTATION: ClientIpcHelper = findImplementation()
+        private val IMPLEMENTATION: ClientIpcHelper = findImplementation()
 
         private fun findImplementation(): ClientIpcHelper {
-            val scanResult = ClassGraph().enableAllInfo().acceptPackages().scan()
+            return Class.forName("com.pylons.ipc.EaselIpcHelper").getConstructor().newInstance() as ClientIpcHelper
+            /*val scanResult = ClassGraph().enableAllInfo().acceptPackages().scan()
             val c = scanResult.getClassesWithAnnotation(Implementation::class.jvmName)
             if (c.size == 0) throw Exception("No registered implementation of ClientIpcHelper in classpath")
             else if (!c[0].extendsSuperclass(ClientIpcHelper::class.qualifiedName))
                 throw Exception("${c[0].name} does not extend ClientIpcHelper")
-            return c[0].loadClass().getConstructor().newInstance() as ClientIpcHelper
+            return c[0].loadClass().getConstructor().newInstance() as ClientIpcHelper*/
         }
 
         fun callWriteString(s: String) {
