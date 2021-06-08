cf9263303aa6120ac57148051ba7d1ef1f6986d5
Ryan <starhelios94@gmail.com>
2021-06-07 20:13:44 -0400

    generateWebLink modified

--- libpylons/src/main/kotlin/tech/pylons/lib/Wallet.kt
+++ libpylons/src/main/kotlin/tech/pylons/lib/Wallet.kt
@@ -402,7 +402,7 @@ abstract class Wallet {
         }
 
         override fun generateWebLink(recipeName: String, recipeId: String): String {
-            return ""
+            return "http://wallet.pylons.tech/?action=purchase_nft&recipe_id=$recipeId&nft_amount=1"
         }
     }
 }
