f9865ac5c5c9303973abe61a9602912fe227a027
sine/cosine/tangent <zfshmn@gmail.com>
2021-07-09 19:26:22 -0700

    recipetool proof-of-concept

---
+++ recipetool/src/main/kotlin/tech.pylons.build/CreateRecipeTask.kt
@@ -0,0 +1,26 @@
+package tech.pylons.build
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.Task
+import org.gradle.api.provider.Property
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.TaskAction
+import tech.pylons.lib.klaxon
+import tech.pylons.wallet.core.Core
+
+abstract class CreateRecipeTask : DefaultTask() {
+    @TaskAction
+    fun createRecipe () {
+        val recipe = RecipeManagementPlugin.getRecipeFromPath(project.property("filepath") as String)
+        Core.current!!.batchCreateRecipe(
+            names = listOf(recipe.name),
+            cookbooks = listOf(recipe.cookbookId),
+            descriptions = listOf(recipe.description),
+            blockIntervals = listOf(recipe.blockInterval),
+            coinInputs = listOf(klaxon.toJsonString(recipe.coinInputs)),
+            itemInputs = listOf(klaxon.toJsonString(recipe.itemInputs)),
+            outputTables = listOf(klaxon.toJsonString(recipe.entries)),
+            outputs = listOf(klaxon.toJsonString(recipe.outputs))
+        )
+    }
+}
--- recipetool/src/main/kotlin/tech.pylons.build/RecipeManagementPlugin.kt
+++ recipetool/src/main/kotlin/tech.pylons.build/RecipeManagementPlugin.kt
@@ -4,15 +4,18 @@ import com.beust.klaxon.Klaxon
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.spongycastle.jce.provider.BouncyCastleProvider
-import pylons.Pylons
 import tech.pylons.ipc.FakeIPC
 import tech.pylons.ipc.FakeUI
 import tech.pylons.ipc.IPCLayer
 import tech.pylons.ipc.UILayer
 import tech.pylons.lib.core.IMulticore
+import tech.pylons.lib.klaxon
 import tech.pylons.lib.logging.Logger
 import tech.pylons.lib.types.Backend
 import tech.pylons.lib.types.Config
+import tech.pylons.lib.types.Cookbook
+import tech.pylons.lib.types.PylonsSECP256K1
+import tech.pylons.lib.types.tx.recipe.Recipe
 import tech.pylons.wallet.core.Core
 import tech.pylons.wallet.core.Multicore
 import java.io.File
@@ -24,39 +27,75 @@ import java.nio.file.Paths
 import java.security.Security
 
 class RecipeManagementPlugin : Plugin<Project> {
-    val klaxon = Klaxon()
+    companion object {
+        var target : Project? = null
 
-    override fun apply(target: Project) {
+        fun getCookbookFromPath (fileName : String) : Cookbook {
+            val path = Path.of(target!!.projectDir.absolutePath, "cookbook", fileName)
+            val f = File(path.toUri())
+            val stream = FileInputStream(f)
+            val cb = try {
+                klaxon.parse<Cookbook>(stream)
+            } catch (e : IOException) {
+                println("${f.name} is not a valid cookbook!")
+                null
+            }
+            stream.close()
+            return cb!!
+        }
+
+        fun getRecipeFromPath (fileName : String) : Recipe {
+            val path = Path.of(target!!.projectDir.absolutePath, "recipe", fileName)
+            val f = File(path.toUri())
+            val stream = FileInputStream(f)
+            val recipe = try {
+                klaxon.parse<Recipe>(stream)
+            } catch (e : IOException) {
+                println("${f.name} is not a valid recipe!")
+                null
+            }
+            stream.close()
+            return recipe!!
+        }
+    }
+
+    override fun apply(t: Project) {
         if (IMulticore.config == null) {
+            target = t
             IPCLayer.implementation = FakeIPC()
             UILayer.implementation = FakeUI()
             Security.addProvider(BouncyCastleProvider())
-            Multicore.enable(Config(Backend.LIVE_DEV, listOf("http://127.0.0.1:1317")))
-            // how do we get our keys?
+            Multicore.enable(Config(Backend.LIVE_DEV, listOf(t.property("addr") as String)))
+            // TODO: There should be a proper system in place for handling keydata,
+            // but the build-out would take too much time atm, so we're gonna do something shitty.
+            if (!File(target!!.projectDir.absolutePath,"dev-keys").exists()) {
+                val kp = PylonsSECP256K1.KeyPair.random()
+                kp.store(Path.of(target!!.projectDir.absolutePath, "dev-keys"))
+            }
+            Multicore.addCore(PylonsSECP256K1.KeyPair.load(Path.of(target!!.projectDir.absolutePath,"dev-keys")))
         }
+        target!!.tasks.create("createRecipe", CreateRecipeTask::class.java)
+        target!!.tasks.create("createCookbook", CreateCookbookTask::class.java)
 
-        //target.extensions.add("recipe-management-plugin", )
-
-        target.task("enumerateRecipes") {
-            val path = Path.of(target.projectDir.absolutePath, "recipe")
+/*        target!!.task("enumerateRecipes") {
+            val path = Path.of(target!!.projectDir.absolutePath, "recipe")
             println("Enumerating recipes in $path")
             getRecipes(path.toString()).forEach {
                 println("Found ${it.cookbookID} / ${it.name}")
             }
         }
-
-        target.task("updateManagedRecipes") {
-            val path = Path.of(target.projectDir.absolutePath, "recipe")
+        target!!.task("updateManagedRecipes") {
+            val path = Path.of(target!!.projectDir.absolutePath, "recipe")
             println("Enumerating recipes in $path")
             getRecipes(path.toString()).forEach {
                 println("Found ${it.cookbookID} / ${it.name}")
                 println(Core.current?.getRecipe(it.id)?.name)
                 //Core.current!!.batchCreateRecipe(listOf(it.name))
             }
-        }
+        }*/
     }
 
-    private fun getRecipes (path : String) : List<Pylons.Recipe> {
+/*    private fun getRecipes (path : String) : List<Pylons.Recipe> {
         val list = mutableListOf<Pylons.Recipe>()
         File(path).walk().forEach {
             println(it.absolutePath)
@@ -73,5 +112,5 @@ class RecipeManagementPlugin : Plugin<Project> {
             }
         }
         return list
-    }
+    }*/
 }
