8d5ff3a52d513b738076c7d504c31e5c9a80fa9a
Tiny <development0305@gmail.com>
2021-07-20 13:22:32 -0400

    protocol prefix missing

--- walletcore/src/main/kotlin/tech/pylons/wallet/core/constants/PylonsNet.kt
+++ walletcore/src/main/kotlin/tech/pylons/wallet/core/constants/PylonsNet.kt
@@ -1,6 +1,6 @@
 package tech.pylons.wallet.core.constants
 
-const val TESTNET_TX_URL = "testnet.pylons.tech"
-const val TESTNET_API_URL = "api.testnet.pylons.tech"
+const val TESTNET_TX_URL = "https://testnet.pylons.tech"
+const val TESTNET_API_URL = "https://api.testnet.pylons.tech"
 const val MAINNET_TX_URL = "NOT IMPLEMENTED"
 const val MAINNET_API_URL = "NOT IMPLEMENTED"
