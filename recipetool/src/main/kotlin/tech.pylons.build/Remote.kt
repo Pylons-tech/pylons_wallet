6dbc71f0cc542af6bfb9dbd4ffe7750328617750
ninja-dev <demarcusr010@gmail.com>
2021-11-05 15:22:45 -0400

    update endpoints with the testnet server urls

--- recipetool/src/main/kotlin/tech.pylons.build/Remote.kt
+++ recipetool/src/main/kotlin/tech.pylons.build/Remote.kt
@@ -5,7 +5,7 @@ import tech.pylons.lib.types.Backend
 data class Remote (val backend : Backend, val chainId : String, val txsUrl : String, val queryUrl : String) {
     companion object {
         val local = Remote(Backend.MANUAL,"pylons", "127.0.0.1:1317", "127.0.0.1:1317")
-        val testnet = Remote(Backend.TESTNET,"pylons-testnet", "rpc.pylons.smartnodes.co", "api.pylons.smartnodes.co")
+        val testnet = Remote(Backend.TESTNET,"pylons-testnet", "rpc.testnet.pylons.tech:26657", "api.testnet.pylons.tech:1317")
     }
 
     fun identifier () : String =
--- walletcore/src/main/kotlin/tech/pylons/wallet/core/constants/PylonsNet.kt
+++ walletcore/src/main/kotlin/tech/pylons/wallet/core/constants/PylonsNet.kt
@@ -1,6 +1,6 @@
 package tech.pylons.wallet.core.constants
 
-const val TESTNET_TX_URL = "https://rpc.pylons.smartnodes.co"
-const val TESTNET_API_URL = "https://api.pylons.smartnodes.co"
+const val TESTNET_TX_URL = "http://rpc.testnet.pylons.tech:26657"       //"https://rpc.pylons.smartnodes.co"
+const val TESTNET_API_URL = "http://api.testnet.pylons.tech:1317"        //"https://api.pylons.smartnodes.co"
 const val MAINNET_TX_URL = "NOT IMPLEMENTED"
 const val MAINNET_API_URL = "NOT IMPLEMENTED"
